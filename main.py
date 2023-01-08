09df4671bc3b94b40fc3446237db9d20111523be
michael-hoang <mike081522@gmail.com>
2023-01-08 13:20:54 -0800

    Refactor asking user for year into a function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+import datetime as dt
+
+
+def ask_for_year() -> str:
+    """Ask user for the year to time travel back to."""
+
+    year = input(
+        'Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n')
+
+
+year = ask_for_year()
