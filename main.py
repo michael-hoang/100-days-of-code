0ab9b8e9147736a607ad6f9c9f997acda5e5f62f
michael-hoang <mike081522@gmail.com>
2023-01-08 15:03:07 -0800

    Retrieve h3 tags of top 100 songs

--- main.py
+++ main.py
@@ -20,8 +20,13 @@ def ask_for_year() -> str:
         ask_for_year()
 
 
-year = ask_for_year()
+# year = ask_for_year()
 url = 'https://www.billboard.com/charts/hot-100/2000-08-12/'
 response = requests.get(url)
 markup = response.text
 soup = BeautifulSoup(markup, 'html.parser')
+
+divs = soup.findAll('div', class_='o-chart-results-list-row-container')
+class_no1 = 'c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet'
+class_no2to100 = 'c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only'
+top100Songs = [div.find('h3', attrs={'class': [class_no1, class_no2to100]}) for div in divs]
