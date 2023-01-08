dbf70f94624d985fcfde2b8176a156edcc4b2865
michael-hoang <mike081522@gmail.com>
2023-01-08 13:23:46 -0800

    Add date input validation and recursion to function

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import datetime as dt
 
 
 def ask_for_year() -> str:
-    """Ask user for the year to time travel back to."""
+    """Recursively ask user for the year to time travel back to."""
 
     year = input(
         'Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n')
+
+    # Date input validation
+    date_format = '%Y-%m-%d'  # YYYY-MM-DD
+    try:
+        dateObject = dt.datetime.strptime(year, date_format)
+        # print(dateObject)
+        return year
+    except ValueError:
+        print('Incorrect date format. Should be YYYY-MM-DD')
+        ask_for_year()
 
 
 year = ask_for_year()
