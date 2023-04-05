8d05041888bc8788447724979d70ec410d8a9740
michael-hoang <mike081522@gmail.com>
2023-04-04 23:53:02 -0700

    Import required Selenium modules

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
