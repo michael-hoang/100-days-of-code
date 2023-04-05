225fed732dda1e7c81b49053cdf72311f37c9a29
michael-hoang <mike081522@gmail.com>
2023-04-04 23:54:01 -0700

    Add Cookie Clicker game url and path to webdriver

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
+
+
+url = 'https://orteil.dashnet.org/cookieclicker/'
+webdriver_path = r'C:\Users\Mike\OneDrive\Desktop\edgedriver_win64\msedgedriver.exe'
+
