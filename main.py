755c8a7902ad598ed802d49b0d67f389ad5cfd89
michael-hoang <mike081522@gmail.com>
2023-04-05 01:36:51 -0700

    Create method to get upgrade prices

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ import time
 
 UPGRADE_EVERY_SECONDS = 5
 
+def get_upgrade_prices() -> list:
+    """Get updated prices for available upgrades from Store."""
+    prices = []
+    unlocked_ugrades = driver.find_elements(By.CSS_SELECTOR, '.product.unlocked.enabled')
+    for element in unlocked_ugrades:
+        price = int(element.find_element(By.CSS_SELECTOR, 'span .price').text)
+        prices.append(price)
+
+    return prices
+
+def upgrade():
+    """Perform action chains on cursor to purchase upgrades from Store."""
+    upgrade_prices = get_upgrade_prices()
+    most_expensive = upgrade_prices.index(max(upgrade_prices))
+    
+
 url = 'https://orteil.dashnet.org/cookieclicker/'
 webdriver_path = r'C:\Users\Mike\OneDrive\Desktop\edgedriver_win64\msedgedriver.exe'
 
@@ -20,8 +36,10 @@ driver.get(url)
 action = ActionChains(driver)
 time.sleep(10.0) # Give the game 10 seconds to load before executing program.
 cookie_btn = driver.find_element(By.CSS_SELECTOR, '#bigCookie')
-cursor_div = driver.find_element(By.CSS_SELECTOR, '#product0')
-grandma_div = driver.find_element(By.CSS_SELECTOR, '#product1')
+p0_div = driver.find_element(By.CSS_SELECTOR, '#product0') # Cursor
+p1_div = driver.find_element(By.CSS_SELECTOR, '#product1') # Grandma
+p2_div = driver.find_element(By.CSS_SELECTOR, '#product2') # Farm
+grandma_div = driver.find_element(By.CSS_SELECTOR, '#product3') # Mine
 
 start_time = time.time()
 track_time = start_time
