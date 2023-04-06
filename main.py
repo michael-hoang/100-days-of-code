a506da2fcea2b45002a6028e1eb0323955c4c522
michael-hoang <mike081522@gmail.com>
2023-04-06 00:22:18 -0700

    Instantiate Service, Options, and webdriver

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 import time
+
+
+url = 'https://www.linkedin.com/'
+webdriver_path = r'C:\Users\Mike\OneDrive\Desktop\edgedriver_win64\msedgedriver.exe'
+
+service = Service(webdriver_path)
+edge_options = Options()
+edge_options.add_experimental_option('detach', True)
+driver = webdriver.Edge(service=service, options=edge_options)
+driver.get(url)
+
