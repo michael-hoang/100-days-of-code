1f425b7c2dddde09e3d7898e1ad1110bdbd529de
michael-hoang <mike081522@gmail.com>
2023-04-06 00:19:25 -0700

    Import required Selenium modules

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+import time
