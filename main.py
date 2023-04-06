baa4c74d3ce87e1d20c1cbac7eae138adf0d1b3a
michael-hoang <mike081522@gmail.com>
2023-04-05 21:15:38 -0700

    Create upgrade function

--- main.py
+++ main.py
@@ -8,21 +8,14 @@ import time
 
 UPGRADE_EVERY_SECONDS = 5
 
-def get_upgrade_prices() -> list:
-    """Get updated prices for available upgrades from Store."""
-    prices = []
-    unlocked_ugrades = driver.find_elements(By.CSS_SELECTOR, '.product.unlocked.enabled')
-    for element in unlocked_ugrades:
-        price = int(element.find_element(By.CSS_SELECTOR, 'span .price').text)
-        prices.append(price)
-
-    return prices
 
 def upgrade():
-    """Perform action chains on cursor to purchase upgrades from Store."""
-    upgrade_prices = get_upgrade_prices()
-    most_expensive = upgrade_prices.index(max(upgrade_prices))
-    
+    """Click and purchase the most expensive upgrade available from Store."""
+    avail_upgrades = driver.find_elements(
+        By.CSS_SELECTOR, '.product.unlocked.enabled')
+    most_expensive_upgrade_avail = avail_upgrades[-1]
+    most_expensive_upgrade_avail.click()
+
 
 url = 'https://orteil.dashnet.org/cookieclicker/'
 webdriver_path = r'C:\Users\Mike\OneDrive\Desktop\edgedriver_win64\msedgedriver.exe'
@@ -34,19 +27,20 @@ driver = webdriver.Edge(service=service, options=edge_options)
 driver.get(url)
 
 action = ActionChains(driver)
-time.sleep(10.0) # Give the game 10 seconds to load before executing program.
+time.sleep(10.0)  # Give the game 10 seconds to load before executing program.
 cookie_btn = driver.find_element(By.CSS_SELECTOR, '#bigCookie')
-p0_div = driver.find_element(By.CSS_SELECTOR, '#product0') # Cursor
-p1_div = driver.find_element(By.CSS_SELECTOR, '#product1') # Grandma
-p2_div = driver.find_element(By.CSS_SELECTOR, '#product2') # Farm
-grandma_div = driver.find_element(By.CSS_SELECTOR, '#product3') # Mine
+p0_div = driver.find_element(By.CSS_SELECTOR, '#product0')  # Cursor
+p1_div = driver.find_element(By.CSS_SELECTOR, '#product1')  # Grandma
+p2_div = driver.find_element(By.CSS_SELECTOR, '#product2')  # Farm
+grandma_div = driver.find_element(By.CSS_SELECTOR, '#product3')  # Mine
 
 start_time = time.time()
 track_time = start_time
 while True:
     cookie_btn.click()
     current_time = time.time()
-    cursor_price = int(driver.find_element(By.CSS_SELECTOR, '#productPrice0').text)
+    cursor_price = int(driver.find_element(
+        By.CSS_SELECTOR, '#productPrice0').text)
     if current_time - track_time >= UPGRADE_EVERY_SECONDS:
         action.move_to_element(cursor_div).click().perform()
         track_time = current_time
