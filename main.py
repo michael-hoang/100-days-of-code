ee084aaf2c96d88baa97828ed8cce06b7c3c7763
michael-hoang <mike081522@gmail.com>
2023-04-06 00:38:42 -0700

    Get reference to sign in button via XPATH

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ edge_options.add_experimental_option('detach', True)
 driver = webdriver.Edge(service=service, options=edge_options)
 driver.get(url)
 
+
+# Log in
 email_input = driver.find_element(By.CSS_SELECTOR, '#session_key')
 password_input = driver.find_element(By.CSS_SELECTOR, '#session_password')
+sign_in_btn = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button')
 
-email_input.send_keys('myemail')
-password_input.send_keys('mypassword')
+# email_input.send_keys('myemail')
+# password_input.send_keys('mypassword')
+# sign_in_btn.click()
