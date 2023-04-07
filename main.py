d1e34eda3e818cdd43d5c991e9e3465ae66d3785
michael-hoang <mike081522@gmail.com>
2023-04-06 20:14:30 -0700

    Add search jobs functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os, time
+import os
+import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -6,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
-
 
 
 url = 'https://www.linkedin.com/'
@@ -29,6 +29,16 @@ password_input = driver.find_element(By.CSS_SELECTOR, '#session_password')
 sign_in_btn = driver.find_element(
     By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button')
 
-email_input.send_keys()
-password_input.send_keys('mypassword')
+email_input.send_keys(email)
+password_input.send_keys(password)
 sign_in_btn.click()
+
+# Search jobs
+jobs_btn = driver.find_element(By.LINK_TEXT, 'Jobs')
+jobs_btn.click()
+time.sleep(3)  # Allow page to load all elements
+search_input = driver.find_element(
+    By.CSS_SELECTOR, 'input.jobs-search-box__text-input')
+search_input.send_keys('python developer')
+time.sleep(1)
+search_input.send_keys(Keys.ENTER)
