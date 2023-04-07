ca1634057d2275c6323b1605c223c394873653c0
michael-hoang <mike081522@gmail.com>
2023-04-06 19:36:32 -0700

    Load environment variables

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os, time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -5,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
-import time
+
 
 
 url = 'https://www.linkedin.com/'
@@ -17,6 +18,10 @@ edge_options.add_experimental_option('detach', True)
 driver = webdriver.Edge(service=service, options=edge_options)
 driver.get(url)
 
+# Load environment variables
+load_dotenv()
+email = os.environ.get('EMAIL')
+password = os.environ.get('PASSWORD')
 
 # Log in
 email_input = driver.find_element(By.CSS_SELECTOR, '#session_key')
@@ -24,6 +29,6 @@ password_input = driver.find_element(By.CSS_SELECTOR, '#session_password')
 sign_in_btn = driver.find_element(
     By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button')
 
-# email_input.send_keys('myemail')
-# password_input.send_keys('mypassword')
-# sign_in_btn.click()
+email_input.send_keys()
+password_input.send_keys('mypassword')
+sign_in_btn.click()
