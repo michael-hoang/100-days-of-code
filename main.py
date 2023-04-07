0da35b374333d454c84907dd9affcf6cd78c8ea8
michael-hoang <mike081522@gmail.com>
2023-04-06 19:32:17 -0700

    Import load_dotenv and create .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -20,7 +21,8 @@ driver.get(url)
 # Log in
 email_input = driver.find_element(By.CSS_SELECTOR, '#session_key')
 password_input = driver.find_element(By.CSS_SELECTOR, '#session_password')
-sign_in_btn = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button')
+sign_in_btn = driver.find_element(
+    By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button')
 
 # email_input.send_keys('myemail')
 # password_input.send_keys('mypassword')
