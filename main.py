d5c3fc2ec479e07c3730c71aca0e8ab2df6e3f3f
michael-hoang <mike081522@gmail.com>
2023-04-07 21:52:35 -0700

    Remove unused modules

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import os
 import time
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 
