9a8b87b1af432c865d22dc1b7342257bf079ff6d
michael-hoang <mike081522@gmail.com>
2023-04-07 20:03:23 -0700

    Load environment variables, set paths

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 
+
+load_dotenv()
+
+URL = 'https://www.pythonanywhere.com/'
+WEBDRIVER_PATH = r'C:\Users\Mike\OneDrive\Desktop\edgedriver_win64\msedgedriver.exe'
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('PASSWORD')
