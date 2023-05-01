d0af66e0b6e787bf837049a00738fb20014d29fc
michael-hoang <mike081522@gmail.com>
2023-04-30 18:09:47 -0700

    Create class TwitterComplaintBot

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class TwitterComplaintBot:
+    """
+    A class representing a Twitter bot that complains to the user's ISP on
+    Twitter if the user's current internet speed doesn't match at least the
+    internet speed guaranteed by the ISP.
+    """
+
+
+if __name__ == '__main__':    
+    PROMISED_DOWN = 
+    PROMISED_UP = 
+    DRIVER_PATH = 
