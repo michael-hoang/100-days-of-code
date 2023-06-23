62ece36bd41c72e49b29718dff2a43c8691d54c5
michael-hoang <mike081522@gmail.com>
2023-06-22 17:33:40 -0700

    Add libraries

--- 53-data-entry-job-automation-CAPSTONE/main.py
+++ 53-data-entry-job-automation-CAPSTONE/main.py
@@ -1,1 +1,13 @@
+import os
+import time
+
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+
+
 google_form_url = 'https://docs.google.com/forms/d/e/1FAIpQLSdIBMqzYh4TDWwAUin8oxXYGL4xZQz7Jnc7cmFg_cX0gnV32w/viewform?usp=sf_link'
