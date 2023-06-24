7d14f694d25be04ab53450c14d86bb1d526d4742
michael-hoang <mike081522@gmail.com>
2023-06-23 21:21:30 -0700

    Enable bot to wait for input elements to be clickable

--- 53-data-entry-job-automation-CAPSTONE/main.py
+++ 53-data-entry-job-automation-CAPSTONE/main.py
@@ -8,6 +8,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class EdgeBrowserBot:
@@ -58,7 +60,9 @@ class EdgeBrowserBot:
                 "ERROR get_html: Need to instantiate Webdriver object using the set_driver method."
             )
         else:
-            time.sleep(2)
+            element = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//input[@type='text']"))
+            )
             inputs = self.driver.find_elements(By.XPATH, "//input[@type='text']")
             inputs[0].send_keys(address)
             inputs[1].send_keys(price)
@@ -120,8 +124,10 @@ if __name__ == "__main__":
 
     bot = EdgeBrowserBot()
     bot.set_driver(driver_path)
-    html = bot.get_html(zillow_url)
-    parser = ZillowParser()
-    parser.set_parser(html)
-    listings = parser.get_listings()
+    # html = bot.get_html(zillow_url)
+
+    # parser = ZillowParser()
+    # parser.set_parser(html)
+    # listings = parser.get_listings()
+
     bot.fill_google_form(google_form_url, "link", "price", "address")
