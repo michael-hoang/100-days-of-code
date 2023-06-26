c5a29532b145c1f6c9704b1c1db8432c265bb176
michael-hoang <mike081522@gmail.com>
2023-06-25 19:49:53 -0700

    Add h1 and gif

--- 55-higher-or-lower-url/server.py
+++ 55-higher-or-lower-url/server.py
@@ -3,5 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
+def display_h1_gif():
+    gif = "<img src='./static/numbers.gif' alt='flashing-numbers-0-through-9'>"
+    return "<h1>Guess a number between 0 and 9</h1>" + gif
+
+
 if __name__ == "__main__":
     app.run(debug=True)
