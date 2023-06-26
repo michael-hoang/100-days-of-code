1c81d96b711b37f029480f6c6a52f6218e9f38ea
michael-hoang <mike081522@gmail.com>
2023-06-25 22:17:50 -0700

    Add .gif's and detect the number entered

--- 55-higher-or-lower-url/server.py
+++ 55-higher-or-lower-url/server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import random
+
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
@@ -6,7 +8,29 @@ app = Flask(__name__)
 @app.route("/")
 def display_h1_gif():
     gif = "<img src='./static/numbers.gif' alt='flashing-numbers-0-through-9'>"
+    global secret_number
+    secret_number = random.randint(0, 9)
+    print(secret_number)
     return "<h1>Guess a number between 0 and 9</h1>" + gif
+
+
+@app.route("/<number>")
+def get_number(number):
+    if not number.isdigit():
+        return redirect("/")
+
+    number = int(number)
+    if number < secret_number:
+        message = "Too low, try again!"
+        image = "./static/low.gif"
+    elif number > secret_number:
+        message = "Too high, try again!"
+        image = "./static/high.gif"
+    elif number == secret_number:
+        message = "You found me!"
+        image = "./static/correct.gif"
+
+    return f"<h1>{message}</h1><img src='{image}' alt='puppy-img'>"
 
 
 if __name__ == "__main__":
