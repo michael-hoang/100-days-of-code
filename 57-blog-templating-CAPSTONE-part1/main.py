5e43a74eaa1e2e8f3d4216358a7d5252705ee767
michael-hoang <mike081522@gmail.com>
2023-06-27 18:42:41 -0700

    Request npoint api and pass all_posts to index.html

--- 57-blog-templating-CAPSTONE-part1/main.py
+++ 57-blog-templating-CAPSTONE-part1/main.py
@@ -1,11 +1,18 @@
+import requests
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template("index.html")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    if response.status_code == 200:
+        all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
