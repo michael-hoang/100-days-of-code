ee4f938567e140ffa76d34f2c14fbec7f48fd0b8
michael-hoang <mike081522@gmail.com>
2023-07-13 14:45:27 -0700

    Add API link to blog posts, pass json data to home page

--- 59-blog-styling-CAPSTONE-part2/main.py
+++ 59-blog-styling-CAPSTONE-part2/main.py
@@ -1,15 +1,25 @@
+import requests
+
 from flask import Flask, render_template
+
+SAMPLE_BLOG_API = "https://api.npoint.io/8f8112d0970cce098676"
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    response = requests.get(SAMPLE_BLOG_API)
+    if response.status_code == 200:
+        blogs = response.json()
+
+    return render_template("index.html", blogs=blogs)
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/contact")
 def contact():
