9468726816366c08300a0581c7aa3500f1084308
michael-hoang <mike081522@gmail.com>
2023-07-13 20:03:46 -0700

    Add post request to form

--- 60-flask-contact-forms/main.py
+++ 60-flask-contact-forms/main.py
@@ -8,5 +8,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/login", methods=["POST"])
+def login():
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
