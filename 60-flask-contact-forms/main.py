fd6d1b8adf0ea679dd10b7510e5a5e191fcb00ef
michael-hoang <mike081522@gmail.com>
2023-07-14 03:03:03 -0700

    Add Day 60 starting files

--- 60-flask-contact-forms/main.py
+++ 60-flask-contact-forms/main.py
@@ -1,16 +1,34 @@
+import requests
+
 from flask import Flask, render_template
+
+SAMPLE_BLOG_API = "https://api.npoint.io/8f8112d0970cce098676"
+response = requests.get(SAMPLE_BLOG_API)
+blogs = response.json()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", blogs=blogs)
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<title>")
+def post(title):
+    for blog in blogs:
+        if blog["title"] == title:
+            return render_template("post.html", blog=blog)
 
 
 if __name__ == "__main__":
