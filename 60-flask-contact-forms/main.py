4a76e1333c753843aff07e453e57980300f9abdf
michael-hoang <mike081522@gmail.com>
2023-07-14 14:32:24 -0700

    Connect contact form to /form-entry route

--- 60-flask-contact-forms/main.py
+++ 60-flask-contact-forms/main.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 SAMPLE_BLOG_API = "https://api.npoint.io/8f8112d0970cce098676"
 response = requests.get(SAMPLE_BLOG_API)
@@ -29,6 +29,16 @@ def post(id):
     for blog in blogs:
         if blog["id"] == id:
             return render_template("post.html", blog=blog)
+
+
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+    return "<h1>Successfully sent your message.</h1>"
 
 
 if __name__ == "__main__":
