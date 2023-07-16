b27377ca6743eb901c35ac74ed614c60768726fc
michael-hoang <mike081522@gmail.com>
2023-07-15 18:54:50 -0700

    Create helpers.py, import environment variables

---
+++ 60-flask-contact-forms/helpers.py
@@ -0,0 +1,5 @@
+import smtplib
+
+
+def send_gmail():
+    """Send Gmail message with the provided login and message."""
--- 60-flask-contact-forms/main.py
+++ 60-flask-contact-forms/main.py
@@ -1,5 +1,6 @@
 import requests
 
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 
 SAMPLE_BLOG_API = "https://api.npoint.io/8f8112d0970cce098676"
