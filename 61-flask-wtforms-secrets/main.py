43e4096bd810e444464bfcb44c1d88beb95245c5
michael-hoang <mike081522@gmail.com>
2023-07-16 23:57:44 -0700

    Receive form data and verify if admin login

--- 61-flask-wtforms-secrets/main.py
+++ 61-flask-wtforms-secrets/main.py
@@ -9,13 +9,15 @@ from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
-    pw = PasswordField("Password", validators=[DataRequired(), Length(9)])
+    pw = PasswordField("Password", validators=[DataRequired(), Length(8)])
     submit = SubmitField("Log In")
 
 
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.getenv("SECRET_KEY")
+EMAIL = "admin@email.com"
+PASSWORD = "12345678"
 
 
 @app.route("/")
@@ -26,7 +28,12 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == EMAIL and login_form.pw.data == PASSWORD:
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
     return render_template("login.html", form=login_form)
 
 
