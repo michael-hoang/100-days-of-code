055e29ba838181a7580da74c7eeb5984089dc00d
michael-hoang <mike081522@gmail.com>
2023-07-20 15:53:17 -0700

    Add sample movies to database

--- 64-top-10-movies/main.py
+++ 64-top-10-movies/main.py
@@ -8,9 +8,9 @@ import requests
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-ratings.db"
 Bootstrap5(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-ratings.db"
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -19,17 +19,41 @@ class Movie(db.Model):
     """An ORM-based model of an SQL table called 'movies'."""
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.VARCHAR(length=250), nullable=False, unique=True)
+    title = db.Column(db.VARCHAR(length=250), nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.VARCHAR(length=1000))
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.VARCHAR(length=1000))
-    img_url = db.Column(db.VARCHAR(length=2048))
+    description = db.Column(db.VARCHAR(length=1000), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.VARCHAR(length=1000), nullable=True)
+    img_url = db.Column(db.VARCHAR(length=2048), nullable=False)
 
 
 with app.app_context():
     db.create_all()
+
+    # new_movie = Movie(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg",
+    # )
+
+second_movie = Movie(
+    title="Avatar The Way of Water",
+    year=2022,
+    description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+    rating=7.3,
+    ranking=9,
+    review="I liked the water.",
+    img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+)
+
+with app.app_context():
+    db.session.add(second_movie)
+    db.session.commit()
 
 
 @app.route("/")
@@ -38,4 +62,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
