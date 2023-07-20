5e01990ad59ada7425c8770809165dc121bae6b8
michael-hoang <mike081522@gmail.com>
2023-07-19 21:27:37 -0700

    Create /edit route, add 'Edit Rating' anchor tag to each book

--- 63-virtual-bookshelf-sqlite-sqlalchemy/main.py
+++ 63-virtual-bookshelf-sqlite-sqlalchemy/main.py
@@ -45,5 +45,13 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        pass
+
+    return render_template("edit.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
