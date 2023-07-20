dbf2014ab1dce371dd38b18efeba5b6955ab512f
michael-hoang <mike081522@gmail.com>
2023-07-20 15:06:22 -0700

    Create class Movie

--- 64-top-10-movies/main.py
+++ 64-top-10-movies/main.py
@@ -8,11 +8,28 @@ import requests
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
-app.config["SQLACHEMY_DATABASE_URI"] = "sqlite:///movies-ratings.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-ratings.db"
 Bootstrap5(app)
 
 db = SQLAlchemy()
 db.init_app(app)
+
+
+class Movie(db.Model):
+    """An ORM-based model of an SQL table called 'movies'."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.VARCHAR(length=250), nullable=False, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.VARCHAR(length=1000))
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.VARCHAR(length=1000))
+    img_url = db.Column(db.VARCHAR(length=2048))
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
