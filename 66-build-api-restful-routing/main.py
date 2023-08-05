83b54aba07bd68ec4aadf58903439c3235b11955
michael-hoang <mike081522@gmail.com>
2023-08-04 17:21:21 -0700

    Create /add route to add new cafe

--- 66-build-api-restful-routing/main.py
+++ 66-build-api-restful-routing/main.py
@@ -105,6 +105,24 @@ def search():
 
 
 ## HTTP POST - Create Record
+@app.route("/add", methods=["GET", "POST"])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
 
 ## HTTP PUT/PATCH - Update Record
 
